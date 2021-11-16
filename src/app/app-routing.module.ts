5389c53b46f47edf18869add0156728fd92cad10
Viết Thuận <jakes.developer@gmail.com>
2021-11-16 22:57:43 +0700

    add
    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
+import { AboutMeComponent } from './modules/about-me/about-me.component';
+import { PortfolioComponent } from './modules/portfolio/portfolio.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   // { path: 'home', component: HomeComponent },
   // { path: 'resume', component: ResumeComponent },
-  // { path: 'about-me', component: AboutMeComponent },
+  { path: 'about-me', component: AboutMeComponent },
   // { path: 'education', component: EducationComponent },
-  // { path: 'portfolio', component: PortfolioComponent },
+  { path: 'portfolio', component: PortfolioComponent },
   // { path: 'activites', component: ActivitiesComponent },
   // { path: 'blog', component: BlogComponent }
 ];
