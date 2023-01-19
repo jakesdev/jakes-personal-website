9c6d4102289cc7e328a934a1c66ddb1425209160
Thuan Nguyen <thuan.nguyen@enlabsoftware.com>
2023-01-19 13:38:48 +0700

    feat: implement husky

--- src/app/features/test-feature/test-feature.component.ts
+++ src/app/features/test-feature/test-feature.component.ts
@@ -1,22 +1,18 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
 
 @Component({
   standalone: true,
-  selector: 'app-test-feature',
-  imports: [
-    CommonModule,
-  ],
-  templateUrl: './test-feature.component.html',
-  styleUrls: ['./test-feature.component.scss'],
-  providers: [
-  ]
+  selector: "app-test-feature",
+  imports: [CommonModule],
+  templateUrl: "./test-feature.component.html",
+  styleUrls: ["./test-feature.component.scss"],
+  providers: [],
 })
 export class TestFeatureComponent {
-
-  currentPosition = '';
+  currentPosition = "";
   mouseMove(event: MouseEvent) {
-    const box = document.getElementById('box1');
+    const box = document.getElementById("box1");
     const boxWidth = box?.offsetWidth;
     const boxHeight = box?.offsetHeight;
 
@@ -24,51 +20,47 @@ export class TestFeatureComponent {
     const axisY = event.y - box?.getBoundingClientRect().y!;
 
     if (axisX === boxWidth! / 2 && axisY === boxHeight! / 2) {
-      console.log('Center');
-      this.currentPosition = 'center'
+      console.log("Center");
+      this.currentPosition = "center";
     }
 
     const positionX = axisX / boxWidth!;
     const positionY = axisY / boxHeight!;
 
-
     if (positionX < 0.5 && positionY < 0.5) {
       if (positionX < positionY) {
-        console.log('Left');
-        this.currentPosition = 'Left'
+        console.log("Left");
+        this.currentPosition = "Left";
       } else {
-        console.log('Top');
-        this.currentPosition = 'Top'
+        console.log("Top");
+        this.currentPosition = "Top";
       }
     }
     if (positionX < 0.5 && positionY > 0.5) {
-      if ((0.5 - positionX) > (positionY - 0.5)) {
-        console.log('Left');
-        this.currentPosition = 'Left'
+      if (0.5 - positionX > positionY - 0.5) {
+        console.log("Left");
+        this.currentPosition = "Left";
       } else {
-        console.log('Bottom');
-        this.currentPosition = 'Bottom'
-
+        console.log("Bottom");
+        this.currentPosition = "Bottom";
       }
     }
-    if ((positionX > 0.5) && positionY < 0.5) {
-
+    if (positionX > 0.5 && positionY < 0.5) {
       if ((0.5 - positionX) * -1 > (positionY - 0.5) * -1) {
-        console.log('Right');
-        this.currentPosition = 'Right'
+        console.log("Right");
+        this.currentPosition = "Right";
       } else {
-        console.log('Top');
-        this.currentPosition = 'Top'
-
+        console.log("Top");
+        this.currentPosition = "Top";
       }
     }
     if (positionX > 0.5 && positionY > 0.5) {
-      if ((0.5 - positionX) * -1 > (positionY - 0.5)) {
-        console.log('Right');
-        this.currentPosition = 'Right'
+      if ((0.5 - positionX) * -1 > positionY - 0.5) {
+        console.log("Right");
+        this.currentPosition = "Right";
       } else {
-        console.log('Bottom');
-        this.currentPosition = 'Bottom'
+        console.log("Bottom");
+        this.currentPosition = "Bottom";
       }
     }
   }
