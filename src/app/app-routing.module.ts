8d7e0232d3759450bd03502fe4ca1709b113f917
Viết Thuận <jakes.developer@gmail.com>
2023-07-13 23:58:10 +0700

    chore: edit sth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,52 +1,46 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { appConfig } from './core/constants';
-import { AdminGuard, IsLoggedInGuard } from './core/guards';
-import { AboutMeComponent } from './features/about-me/about-me.component';
-import { ErrorComponent } from './features/error/error.component';
-import { HomeComponent } from './features/home/home.component';
-import { TestFeatureComponent } from './features/test-feature/test-feature.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { appConfig } from "./core/constants";
+import { AdminGuard, IsLoggedInGuard } from "./core/guards";
+import { AboutMeComponent } from "./features/about-me/about-me.component";
+import { ErrorComponent } from "./features/error/error.component";
+import { HomeComponent } from "./features/home/home.component";
 
 const routes: Routes = [
   {
-    path: 'auth',
+    path: "auth",
     canActivate: [IsLoggedInGuard],
     loadChildren: () =>
-      import('./features/auth/auth.module').then((m) => m.AuthModule)
+      import("./features/auth/auth.module").then((m) => m.AuthModule),
   },
   {
-    path: '',
-    pathMatch: 'full',
-    component: HomeComponent
+    path: "",
+    pathMatch: "full",
+    component: HomeComponent,
   },
   {
     path: appConfig.routes.aboutMe.index,
-    component: AboutMeComponent
+    component: AboutMeComponent,
   },
   {
     path: appConfig.routes.article.index,
     loadChildren: () =>
-      import('./features/article/article.module').then((m) => m.ArticleModule)
+      import("./features/article/article.module").then((m) => m.ArticleModule),
   },
   {
     path: appConfig.routes.admin.index,
     canActivate: [AdminGuard],
     loadChildren: () =>
-      import('./features/admin/admin.module').then((m) => m.AdminModule)
+      import("./features/admin/admin.module").then((m) => m.AdminModule),
   },
   {
-    path: 'test-feature',
-    component: TestFeatureComponent
-  },
-
-  {
-    path: '**',
+    path: "**",
     component: ErrorComponent,
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
